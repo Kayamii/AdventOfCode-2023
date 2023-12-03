4493c7f2547ab688fbde15a63501eae61780b357
kayamii <chbichibfarouk@gmail.com>
2023-12-03 15:06:00 +0100

    Day 2 Solution

--- Day 2/part2.py
+++ Day 2/part2.py
@@ -1,23 +1,25 @@
 import re
-def validate_game(input_string): 
+def max_line(input_string): 
     pattern = r'(\d+) (\w+)'
     matches = re.findall(pattern, input_string)
     game_parts = [(int(number), color) for number, color in matches]
+    max_red=0
+    max_green=0
+    max_blue=0
     for number, color in game_parts:
-        if color == 'red' and number > reds:
-            return False
-        elif color == 'green' and number > greens:
-            return False
-        elif color == 'blue' and number > blues:
-            return False   
-    return True
-i=0
-sum=0
+        if color == 'red' and number > max_red:
+            max_red=number
+        if color == 'green' and number > max_green:
+            max_green=number
+        if color == 'blue' and number > max_blue:
+            max_blue=number
+    return max_red*max_green*max_blue                
+
 file_path = 'c:/Users/mrtng/OneDrive/Bureau/AdventOfCode-2023/Day 2/puzzle.txt'
+sum=0
 with open(file_path, 'r') as f:
     for line in f:
-        is_valid = validate_game(line)
-        i+=1
-        if is_valid:
-            sum+=i
-print(sum)            
+        max= max_line(line)
+        sum+=max
+    print(sum)   
+           
