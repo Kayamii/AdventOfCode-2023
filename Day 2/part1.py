37cf4c53d172f5869ffb921d8e9e62f8c8d969e1
kayamii <chbichibfarouk@gmail.com>
2023-12-03 14:59:31 +0100

    Day 2 Solution

--- Day 2/part1.py
+++ Day 2/part1.py
@@ -1,17 +1,8 @@
-
 import re
-
-def validate_game(input_string, reds=12, greens=13, blues=14):
-    
+def validate_game(input_string, reds=12, greens=13, blues=14):  
     pattern = r'(\d+) (\w+)'
-
-   
-    matches = re.findall(pattern, input_string)
-
-   
+    matches = re.findall(pattern, input_string)  
     game_parts = [(int(number), color) for number, color in matches]
-
-   
     for number, color in game_parts:
         if color == 'red' and number > reds:
             return False
@@ -19,13 +10,10 @@ def validate_game(input_string, reds=12, greens=13, blues=14):
             return False
         elif color == 'blue' and number > blues:
             return False
-
-   
     return True
 i=0
 sum=0
 file_path = 'c:/Users/mrtng/OneDrive/Bureau/AdventOfCode-2023/Day 2/puzzle.txt'
-
 with open(file_path, 'r') as f:
     for line in f:
         is_valid = validate_game(line)
@@ -33,4 +21,3 @@ with open(file_path, 'r') as f:
         if is_valid:
             sum+=i
 print(sum)            
-        
