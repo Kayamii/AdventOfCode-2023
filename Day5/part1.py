4227de593b1d680473ce0ebc4ad98ce9e72e8da1
kayamii <chbichibfarouk@gmail.com>
2023-12-05 19:47:47 +0100

    Day 5 Solution

--- Day5/part1.py
+++ Day5/part1.py
@@ -1,19 +1,31 @@
-seeds, *parts = open("c:/Users/mrtng/OneDrive/Bureau/AdventOfCode-2023/Day5/example.txt").read().split("\n\n")
+file_path = "c:/Users/mrtng/OneDrive/Bureau/AdventOfCode-2023/Day5/example.txt"
 
-seeds = list(map(int, seeds.split(":")[1].split()))
+with open(file_path) as file:
+    file_content = file.read()
+
+seeds_part, *parts = file_content.split("\n\n")
+seeds_string = seeds_part.split(":")[1]
+seeds = list(map(int, seeds_string.split()))
 
 for block in parts:
     ranges = []
-    for line in block.splitlines()[1:]:
-        ranges.append(list(map(int, line.split())))
-    new = []
-    for i in seeds:
+    lines = block.splitlines()[1:]
+
+    for line in lines:
+        range_values = list(map(int, line.split()))
+        ranges.append(range_values)
+
+    new_seeds = []
+
+    for seed in seeds:
         for dest, source, length in ranges:
-            if source <= i < source + length:
-                new.append(i - source + dest)
+            if source <= seed < source + length:
+                transformed_seed = seed - source + dest
+                new_seeds.append(transformed_seed)
                 break
         else:
-            new.append(i)
-    seeds = new
+            new_seeds.append(seed)
+
+    seeds = new_seeds
 
 print(min(seeds))
